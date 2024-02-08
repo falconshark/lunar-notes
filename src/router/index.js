f490463476193cbfd1906a918087fcfda99e5f79
Sardo <sardo.ip@sardo.work>
2024-02-09 00:53:57 +0800

    Do not need login part anymore

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Login from '../views/Login.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'login',
-      component: () => import('../views/Login.vue')
-    },
-    {
-      path: '/dashboard',
       name: 'Dashboard',
       component: () => import('../views/Dashboard.vue')
     },
