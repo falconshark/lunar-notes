b502dd10c5e9de2b31e85b3dcbc6c44f10353b23
Sardo <sardo.ip@sardo.work>
2024-02-09 20:10:20 +0800

    Able to get dropbox auth url

--- src/components/ConnectDropbox.vue
+++ src/components/ConnectDropbox.vue
@@ -5,15 +5,25 @@
         <div class="connect-card-text">
             Oh! It's look like you had not connect any cloud drive yet. <br/>
             Connect it before start using.
-            <a href="#" class="btn btn-primary connect-button">Connect</a>
+            <button @click="conntectDropbox" class="btn btn-primary connect-button">Connect</button>
         </div>
     </div>
   </el-card>
 </template>
   
   <script>
+  import {Dropbox} from 'dropbox';
   export default {
-    name: 'Dashboard',
+    name: 'ConnectDropbox',
+    methods:{
+      async conntectDropbox(){
+        const redirectUrl = `${import.meta.env.VITE_BASE_URL}/`;
+        const clientId = import.meta.env.VITE_DROPBOX_APP_ID;
+        const dbx = new Dropbox({ clientId: clientId });
+        const authUrl = await dbx.auth.getAuthenticationUrl(redirectUrl);
+        console.log(authUrl);
+      },
+    },
   }
   </script>
   
